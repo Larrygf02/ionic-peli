c033aa26e62ce8af9ca49b7a6d73a016d5624785
larrygf02 <raulgf_02@hotmail.com>
2020-02-29 18:46:30 -0500

    Images in app

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RespuestaMBD } from '../../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class MovieService {
   constructor(private http: HttpClient) { }
 
   getFeature() {
-    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=bd288bc52a7e3fe71ffed382296d9f15&language=es&include_image_language=es`)
+    return this.http.get<RespuestaMBD>(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=bd288bc52a7e3fe71ffed382296d9f15&language=es&include_image_language=es`)
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../services/movie.service';
+import { Pelicula } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
@@ -7,10 +8,18 @@ import { MovieService } from '../services/movie.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+  peliculas: Pelicula[] = []
 
+  slideOpts = {
+    slidesPerView: 1.3,
+    freeMode: true
+  }
+  
   constructor(private moviesService: MovieService) {}
   ngOnInit() {
     this.moviesService.getFeature()
-      .subscribe(console.log)
+      .subscribe(resp => {
+        this.peliculas = resp.results
+      })
   }
 }
