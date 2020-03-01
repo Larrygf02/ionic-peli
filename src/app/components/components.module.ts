64ffcb3dd246d8d86e64a52ee309e37dcb207521
larrygf02 <raulgf_02@hotmail.com>
2020-03-01 12:19:19 -0500

    Detalles components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,11 +5,19 @@ import { SlideshowBackdropComponent } from './slideshow-backdrop/slideshow-backd
 import { PipesModule } from '../pipes/pipes.module';
 import { SlideshowPosterComponent } from './slideshow-poster/slideshow-poster.component';
 import { SlideshowParesComponent } from './slideshow-pares/slideshow-pares.component';
+import { DetalleComponent } from './detalle/detalle.component';
 
 
 
 @NgModule({
-  declarations: [SlideshowBackdropComponent, SlideshowPosterComponent, SlideshowParesComponent],
+  entryComponents: [
+    DetalleComponent
+  ],
+  declarations: [
+    SlideshowBackdropComponent,
+    SlideshowPosterComponent,
+    SlideshowParesComponent,
+    DetalleComponent],
   imports: [
     CommonModule,
     IonicModule,
@@ -18,7 +26,8 @@ import { SlideshowParesComponent } from './slideshow-pares/slideshow-pares.compo
   exports: [
     SlideshowBackdropComponent,
     SlideshowPosterComponent,
-    SlideshowParesComponent
+    SlideshowParesComponent,
+    DetalleComponent
   ]
 })
 export class ComponentsModule { }
--- src/app/components/slideshow-poster/slideshow-poster.component.ts
+++ src/app/components/slideshow-poster/slideshow-poster.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Pelicula } from '../../../interfaces/interfaces';
+import { ModalController } from '@ionic/angular';
+import { DetalleComponent } from '../detalle/detalle.component';
 
 @Component({
   selector: 'app-slideshow-poster',
@@ -14,8 +16,16 @@ export class SlideshowPosterComponent implements OnInit {
     slidesPerView: 3.3,
     freeMode: true
   }
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {}
-
+  async verDetalle(id: string) {
+    const modal = await this.modalCtrl.create({
+      component: DetalleComponent,
+      componentProps: {
+        id
+      }
+    });
+    modal.present()
+  }
 }
