61ab076ad2205416433d3bf5fe03dbdf4f5062d9
larrygf02 <raulgf_02@hotmail.com>
2020-03-14 11:38:22 -0500

    Mostrando peliculas buscadas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { MovieService } from '../services/movie.service';
+import { Pelicula } from '../interfaces/interfaces';
+import { ModalController } from '@ionic/angular';
+import { DetalleComponent } from '../components/detalle/detalle.component';
 
 @Component({
   selector: 'app-tab2',
@@ -8,13 +11,34 @@ import { MovieService } from '../services/movie.service';
 })
 export class Tab2Page {
   textoBuscar = ''
+  peliculas: Pelicula[] = []
+  cargarSpinner = false
   ideas: string[] = ['Spiderman', 'Avenger', 'El señor de los anillos']
-  constructor( private movieService:MovieService) {}
+  constructor( private movieService:MovieService, 
+                private modalCtrl: ModalController) {}
   buscar(event) {
+    this.cargarSpinner = true;
     const valor = event.detail.value;
     console.log(valor);
-    this.movieService.buscarPeliculas(valor)
-      .subscribe(resp => console.log(resp))
+    if (valor !== '') {
+      this.movieService.buscarPeliculas(valor)
+        .subscribe(resp => {
+          this.peliculas = resp["results"]
+          this.cargarSpinner = false
+        })
+    }else {
+      this.peliculas = []
+      this.cargarSpinner = false
+    }
+  }
+  async detalle (id: string) {
+      const modal = await this.modalCtrl.create({
+        component: DetalleComponent,
+        componentProps: {
+          id
+        }
+      });
+      modal.present()
   }
 /*   colocarBuscar(event) {
     console.log(event);
