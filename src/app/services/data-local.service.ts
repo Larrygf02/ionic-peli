18c8c9856a98133afec54179da33163cb3d58c0d
larrygf02 <raulgf_02@hotmail.com>
2020-03-14 12:29:21 -0500

    Validar si existe peliculas en el storage

--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { PeliculaDetalle } from '../interfaces/interfaces';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,34 @@ export class DataLocalService {
 
   peliculas: PeliculaDetalle[] = []
 
-  constructor(private storage: Storage ) { }
+  constructor(private storage: Storage,
+              public toastController: ToastController ) { }
 
   guardarPelicula(pelicula: PeliculaDetalle) {
-    this.peliculas.push(pelicula);
+    let existe = false;
+    let mensaje = ''
+    for ( const peli of this.peliculas) {
+      if (peli.id === pelicula.id ) {
+        existe = true
+        break;
+      }
+    }
+    if ( existe ) {
+      this.peliculas = this.peliculas.filter(peli => peli.id !== pelicula.id)
+      mensaje = 'Removido de favoritos'
+    }else {
+      this.peliculas.push(pelicula);
+      mensaje = 'Agregado a favoritos'
+    }
     this.storage.set('peliculas', this.peliculas)
+    this.presentToast(mensaje)
+
+  }
+  async presentToast(mensaje: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000
+    });
+    toast.present()
   }
 }  
