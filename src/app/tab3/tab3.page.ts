4751b3a0dd99bb0d359f2cc8e7dce539443b2b9f
larrygf02 <raulgf_02@hotmail.com>
2020-03-14 13:43:11 -0500

    Peliculas por genero

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -11,14 +11,47 @@ import { MovieService } from '../services/movie.service';
 export class Tab3Page {
 
   peliculas: PeliculaDetalle[] = []
+  pelisGenres = []
+  idsPelisGenres = []
   generos: any[] = []
   constructor(private dataLocal: DataLocalService,
     private moviesService: MovieService) {}
   
   async ngOnInit() {
-    this.peliculas = await this.dataLocal.cargarFavoritos()
+    this.dataLocal.cargarFavoritos().then( resp => {
+      this.peliculas = resp;
+      this.cargarPeliculasGenero()
+      console.log(this.pelisGenres);
+      console.log(this.idsPelisGenres);
+    })
     this.generos = await this.moviesService.cargarGeneros()
     console.log(this.generos);
+    console.log(this.peliculas);
+  }
+
+  cargarPeliculasGenero() {
+    console.log('Cargando');
+    this.peliculas.forEach( peli => {
+      peli.genres.forEach( genre => {
+        if (!this.idsPelisGenres.includes(genre.id)) {
+          this.pelisGenres.push(genre)
+          this.idsPelisGenres.push(genre.id)
+        }
+      })
+    })
+    return true
+  }
+  pelisPorGenero(genero) {
+    return this.peliculas.filter( peli => {
+      let existe = false
+      console.log(existe);
+      peli.genres.forEach( genre => {
+        if (genre.id === genero.id) {
+          existe = true
+        }
+      })
+      return existe;
+    })
   }
 
 }
